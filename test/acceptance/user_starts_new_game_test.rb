03bafe3501e20bce9d34f10e984f7ed7ca7cb1a0
Calaway <calaway@users.noreply.github.com>
2016-12-13 15:57:31 -0700

    Work on new game test.

--- test/acceptance/user_starts_new_game_test.rb
+++ test/acceptance/user_starts_new_game_test.rb
@@ -6,11 +6,10 @@ class UserStartsNewGameTest < Capybara::Rails::TestCase
   # And I select a cohort
   # And I click Start Game
   # Then I am taken to the new game pa
-
+  skip
   def test_it_starts_new_game
     visit root_path
 
-    find(:select, from, options).find(:option, value, options).select_option
     click_button "Start Game"
 
     assert current_path, new_identification_path
