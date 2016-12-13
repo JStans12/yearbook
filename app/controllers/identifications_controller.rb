b4c2dab10cdea109915afdb5dcfb136214c10301
Calaway <calaway@users.noreply.github.com>
2016-12-13 15:57:10 -0700

    Replace people database in identifications controller.

--- app/controllers/identifications_controller.rb
+++ app/controllers/identifications_controller.rb
@@ -1,6 +1,23 @@
 class IdentificationsController < ApplicationController
   def new
-    @identification = Identification.create(:person => Person.random)
+    # checks for people cookie
+    # if none then load people from api and save to cookie
+    # grab random person from cookie
+    def people
+        session[:people] || fetch_people
+    end
+
+    def fetch_people
+      conn = Faraday.new(:url => 'https://census-app-staging.herokuapp.com') do |faraday|
+        faraday.request  :url_encoded             # form-encode POST params
+        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      end
+      response = conn.get '/api/v1/all_users'
+      response.body
+    end
+
+
+    @identification = Identification.create(:person => people.sample)
     redirect_to new_identification_guess_path(@identification)
   end
 end
