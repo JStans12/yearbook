f3e03c0c43a85e9774ab5d48a4fa0569c09433bd
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-12-14 17:03:53 -0700

    Remove home controller and view'

--- app/controllers/identifications_controller.rb
+++ app/controllers/identifications_controller.rb
@@ -1,13 +1,5 @@
 class IdentificationsController < ApplicationController
   def new
-    # checks for people cookie
-    # if none then load people from api and save to cookie
-    # grab random person from cookie
-    # def people
-    #     session[:people] ||= fetch_people
-    # end
-
-    # @identification = Identification.create(:person_id => people.sample["id"])
     @identification = Identification.create(:person => Person.random)
     redirect_to new_identification_guess_path(@identification)
   end
--- test/features/user_starts_new_game_test.rb
+++ test/features/user_starts_new_game_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class UserStartsNewGameTest < Capybara::Rails::TestCase
-  # As a user
-  # When I visit the root path
-  # And I select a cohort
-  # And I click Start Game
-  # Then I am taken to the new game pa
   def test_it_starts_new_game
     visit root_path
 
