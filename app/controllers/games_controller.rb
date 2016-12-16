51a22fc53735e933f68535bf03b43898d11cd314
Calaway <calaway@users.noreply.github.com>
2016-12-15 20:43:55 -0700

    Extract methods from game controller to person model.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,16 +4,6 @@ class GamesController < ApplicationController
 
   def create
     Person.new_game
-
-    person = Person.new
-    person.clear_all_people
-    CensusApiService.fetch_people.each do |person|
-      cohort = Cohort.find_or_create_by(name: person["cohort"])
-      cohort.people.create(first_name: person["first_name"],
-                           last_name: person["last_name"],
-                           photo_url: person["image_url"])
-    end
-
     redirect_to new_identification_path
   end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,12 @@ class Person < ApplicationRecord
   belongs_to :cohort
   has_many :identifications
 
+  def self.new_game
+    person = Person.new
+    person.clear_all_people
+    person.load_people
+  end
+
   def self.random
     random_set(1).first
   end
@@ -14,5 +20,14 @@ class Person < ApplicationRecord
     Person.destroy_all
   end
 
-  
+  def load_people
+    CensusApiService.fetch_people.each do |person|
+      cohort = Cohort.find_or_create_by(name: person["cohort"])
+      cohort.people.create(
+      first_name: person["first_name"],
+      last_name: person["last_name"],
+      photo_url: person["image_url"]
+      )
+    end
+  end
 end
