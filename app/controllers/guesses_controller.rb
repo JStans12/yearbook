da9c11e6134a5ac8f8e96c290c0f00b560294d6b
Calaway <calaway@users.noreply.github.com>
2016-12-22 09:59:41 -0700

    Remove debugger.

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -8,7 +8,7 @@ class GuessesController < ApplicationController
   def create
     @identification = Identification.find(params[:identification_id])
     @guess = @identification.guesses.create(:hypothesis => params[:name])
-require "pry"; binding.pry
+
     if @guess.correct?
       flash[:success] = "Correct: #{params[:name]}!"
       redirect_to new_identification_path
