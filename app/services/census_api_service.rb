72630dd14ad0e560c111db803fd08bb7cfecfec6
Calaway <calaway@users.noreply.github.com>
2016-12-30 13:36:39 -0700

    Get api call to pass hardcoded token and get result successfully.

--- app/services/census_api_service.rb
+++ app/services/census_api_service.rb
@@ -1,9 +1,16 @@
-module CensusApiService
+class CensusApiService
+  def self.url
+    'https://census-app-staging.herokuapp.com'
+  end
+
   def self.fetch_people
-    conn = Faraday.new(:url => 'https://census-app-staging.herokuapp.com') do |faraday|
+    conn = Faraday.new(:url => url) do |faraday|
       faraday.adapter Faraday.default_adapter
     end
-    response = conn.get '/api/v1/users'
+    response = conn.get do |req|
+      req.url '/api/v1/users'
+      req.headers['Authorization'] = 'Bearer 1e71efa360f770c4698a9ea989927b800ed9c2aaa07ca2b414db5ef2052f20ec'
+    end
     JSON.parse(response.body)
   end
 end
