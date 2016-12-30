ac157b3f73aa376e9b5ad323fd0c4c2dcda31f07
Calaway <calaway@users.noreply.github.com>
2016-12-30 14:40:23 -0700

    CensusApiService now fetches the authentication token first and then makes the get request.

--- app/services/census_api_service.rb
+++ app/services/census_api_service.rb
@@ -3,14 +3,28 @@ class CensusApiService
     'https://census-app-staging.herokuapp.com'
   end
 
-  def self.fetch_people
-    conn = Faraday.new(:url => url) do |faraday|
+  def self.conn
+    Faraday.new(:url => url) do |faraday|
       faraday.adapter Faraday.default_adapter
+      faraday.basic_auth(ENV['CENSUS_APPLICATION_ID'], ENV['CENSUS_SECRET'])
     end
+  end
+
+  def self.token
+    response = conn.post do |req|
+      req.url '/oauth/token'
+      req.params['grant_type'] = 'client_credentials'
+    end
+    return false unless response.success?
+    JSON.parse(response.body)['access_token']
+  end
+
+  def self.fetch_people
     response = conn.get do |req|
       req.url '/api/v1/users'
-      req.headers['Authorization'] = 'Bearer 1e71efa360f770c4698a9ea989927b800ed9c2aaa07ca2b414db5ef2052f20ec'
+      req.headers['Authorization'] = "Bearer #{token}"
     end
+    return false unless response.success?
     JSON.parse(response.body)
   end
 end
